ca7e1efd6d681cef012aaa7576b53fa07fe1e6ba
Cecilia Liu <cecilialiu8@gmail.com>
2022-01-11 15:21:57 -0600

    fixing error due to repackage

---
+++ src/main/java/gov/cms/madie/testcase/TestCaseServiceApplication.java
@@ -0,0 +1,12 @@
+package gov.cms.madie.testcase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestCaseServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TestCaseServiceApplication.class, args);
+  }
+}
