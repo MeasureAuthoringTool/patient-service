dbe6da87b17d34700baf63ccc07f84db0df59b08
Cecilia Liu <cecilialiu8@gmail.com>
2022-01-11 09:48:44 -0600

    remove unused imports

--- src/test/java/com/example/template/controller/PatientControllerMvcTest.java
+++ src/test/java/com/example/template/controller/PatientControllerMvcTest.java
@@ -1,15 +1,11 @@
 package com.example.template.controller;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 
 import org.mockito.ArgumentCaptor;
 import org.mockito.Captor;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.verifyNoMoreInteractions;
 import static org.mockito.Mockito.when;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,58 +21,57 @@ import com.example.template.models.Patient;
 import com.example.template.repositories.PatientRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-
 @WebMvcTest({PatientController.class})
 public class PatientControllerMvcTest {
 
   @MockBean private PatientRepository repository;
   @Autowired private MockMvc mockMvc;
   @Captor private ArgumentCaptor<Patient> patientArgumentCaptor;
-  
+
   private Patient patient;
-  
-  
+  private static final String TEST_ID = "TESTID";
+  private static final String TEST_USER = "TestUser";
+  private static final String TEST_USER_2 = "TestUser2";
+
   @BeforeEach
   public void setUp() {
     patient = new Patient();
-    patient.setId("TESTID");
-    patient.setCreatedBy("TestUser");
-    patient.setLastModifiedBy("TestUser2");
+    patient.setId(TEST_ID);
+    patient.setCreatedBy(TEST_USER);
+    patient.setLastModifiedBy(TEST_USER_2);
   }
 
   @Test
   public void testHealth() throws Exception {
     this.mockMvc
-      .perform(MockMvcRequestBuilders.get("/"))
+        .perform(MockMvcRequestBuilders.get("/"))
         .andExpect(MockMvcResultMatchers.status().isOk())
-        	.andExpect(MockMvcResultMatchers.content().string("PatientController is running..."));
+        .andExpect(MockMvcResultMatchers.content().string("PatientController is running..."));
   }
-  
+
   @Test
   public void testNewPatientPassed() throws Exception {
-  	when(repository.save(any(Patient.class))).thenReturn(patient);
-  	
-  	mockMvc.perform( MockMvcRequestBuilders
-	      .post("/patient")
-	      	.content(asJsonString(patient))
-	      		.contentType(MediaType.APPLICATION_JSON)
-	      			.accept(MediaType.APPLICATION_JSON))
-								.andDo(print())
-									.andExpect(MockMvcResultMatchers.status().isCreated());	
-  	
-  	verify(repository, times(1)).save(patientArgumentCaptor.capture());
-  	verifyNoMoreInteractions(repository);
-  	Patient savePatient = patientArgumentCaptor.getValue();
-  	Assertions.assertEquals("TESTID", savePatient.getId());
-  	Assertions.assertEquals("TestUser", savePatient.getCreatedBy());
-  	Assertions.assertEquals("TestUser2", savePatient.getLastModifiedBy());
+    when(repository.save(any(Patient.class))).thenReturn(patient);
+
+    mockMvc
+        .perform(
+            MockMvcRequestBuilders.post("/patient")
+                .content(asJsonString(patient))
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON))
+        .andDo(print())
+        .andExpect(MockMvcResultMatchers.status().isCreated())
+        .andExpect(MockMvcResultMatchers.jsonPath("$.id").value(TEST_ID))
+        .andExpect(MockMvcResultMatchers.jsonPath("$.createdBy").value(TEST_USER))
+        .andExpect(MockMvcResultMatchers.jsonPath("$.lastModifiedBy").value(TEST_USER_2));
+
   }
-  
+
   private String asJsonString(final Object obj) {
     try {
-        return new ObjectMapper().writeValueAsString(obj);
+      return new ObjectMapper().writeValueAsString(obj);
     } catch (Exception e) {
-        throw new RuntimeException(e);
+      throw new RuntimeException(e);
     }
-	}
+  }
 }
