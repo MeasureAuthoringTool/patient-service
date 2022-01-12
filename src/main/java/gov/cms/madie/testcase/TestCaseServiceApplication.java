5ad26c3028eda3c329e485e8023e33840296a04c
Nate Moraca <nate.moraca@semanticbits.com>
2022-01-12 13:03:19 -0500

    add CORS for local dev; ref #MAT-3706

--- src/main/java/gov/cms/madie/testcase/TestCaseServiceApplication.java
+++ src/main/java/gov/cms/madie/testcase/TestCaseServiceApplication.java
@@ -2,11 +2,27 @@ package gov.cms.madie.testcase;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class TestCaseServiceApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(TestCaseServiceApplication.class, args);
+  }
+
+  @Bean
+  public WebMvcConfigurer corsConfigurer() {
+    return new WebMvcConfigurer() {
+      @Override
+      public void addCorsMappings(CorsRegistry registry) {
+        registry
+                .addMapping("/**")
+                .allowedMethods("PUT", "POST", "GET")
+                .allowedOrigins("http://localhost:9000", "https://dev-madie.hcqis.org");
+      }
+    };
   }
 }
