7cf482f5f501ca3e6f33878c7c6508b91c175348
Ashok Dongare <ashok.dongare@semanticbits.com>
2022-01-19 13:05:16 -0500

    MAT-3744 Create Retrieve test cases Endpoint

--- src/main/java/gov/cms/madie/testcase/controller/ControllerUtil.java
+++ src/main/java/gov/cms/madie/testcase/controller/ControllerUtil.java
@@ -2,4 +2,5 @@ package gov.cms.madie.testcase.controller;
 
 public class ControllerUtil {
   public static final String TEST_CASE = "/test-case";
+  public static final String TEST_CASES = "/test-cases";
 }
