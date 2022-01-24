435f99ef0e40e1beec7cce8a58fa0458313c7535
Gregory P. Akins <gregory.akins@semanticbits.com>
2022-01-24 11:09:57 -0500

    MAT-3865: Shelving Test-case as the test-case CRUD operations are being embedded in measure service

--- src/main/java/gov/cms/madie/testcase/controller/TestCaseController.java
+++ src/main/java/gov/cms/madie/testcase/controller/TestCaseController.java
@@ -1,35 +1,6 @@
 package gov.cms.madie.testcase.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import gov.cms.madie.testcase.models.TestCase;
-import gov.cms.madie.testcase.repositories.TestCaseRepository;
-import lombok.RequiredArgsConstructor;
-
-import java.util.List;
-
 @RestController
-@RequiredArgsConstructor
-public class TestCaseController {
-
-  @Autowired private final TestCaseRepository repository;
-
-  @PostMapping(ControllerUtil.TEST_CASE)
-  public ResponseEntity<TestCase> addTestCase(@RequestBody TestCase testCase) {
-    testCase.setId(null);
-    TestCase saveTestCase = repository.save(testCase);
-
-    return ResponseEntity.status(HttpStatus.CREATED).body(saveTestCase);
-  }
-
-  @GetMapping(ControllerUtil.TEST_CASES)
-  public ResponseEntity<List<TestCase>> getTestCases() {
-    return ResponseEntity.ok(repository.findAll());
-  }
-}
+public class TestCaseController {}
